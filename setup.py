abc257782628fb607dced3f41678ed0681bf13f5
methylDragon <methylDragon@gmail.com>
2019-07-09 22:19:27 +0800

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('PyPI/HISTORY.md') as history_file:
     HISTORY = history_file.read()
 
 setup_args = {'name': "cortex2",
-              'version': "1.0.0.post2",
+              'version': "1.0.0.post3",
               'description': "Comprehensive threaded, asynchronous Python client for the Emotiv EEG Cortex 2 API",
               'long_description_content_type': "text/markdown",
               'long_description': README + '\n\n' + HISTORY,
@@ -17,8 +17,7 @@ setup_args = {'name': "cortex2",
               'url': "https://github.com/methylDragon/emotiv-cortex2-python-client",
               'download_url': "https://pypi.org/project/cortex2/"}
 
-install_requires = ['websockets>=7.0',
-                    ]
+install_requires = ['websockets>=7.0']
 
 if __name__ == '__main__':
     setup(**setup_args, install_requires=install_requires)
